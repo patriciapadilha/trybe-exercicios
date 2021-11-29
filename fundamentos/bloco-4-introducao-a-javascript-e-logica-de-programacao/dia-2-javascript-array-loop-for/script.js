3cecacd436074394b1e6053e5b4a9228755097ec
Patricia Padilha <patriciapadilhabio@gmail.com>
2021-11-29 15:06:28 -0300

    nova branch

---
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-2-javascript-array-loop-for/script.js
@@ -0,0 +1,14 @@
+//Exercício 1
+
+let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+
+//1. Nesse primeiro exercício, percorra o array imprimindo todos os valores nele contidos com a função `console.log()` ;
+//2. Para o segundo exercício, some todos os valores contidos no array e imprima o resultado;
+//3. Para o terceiro exercício, calcule e imprima a média aritmética dos valores contidos no array;
+//- A média aritmética é o resultado da soma de todos os elementos divido pelo número total de elementos.
+//1. Com o mesmo código do exercício anterior, caso o valor final seja maior que 20, imprima a mensagem: "valor maior que 20". Caso não seja, imprima a mensagem: "valor menor ou igual a 20";
+//2. Utilizando `for` , descubra qual o maior valor contido no array e imprima-o;
+//3. Descubra quantos valores ímpares existem no array e imprima o resultado. Caso não exista nenhum, imprima a mensagem: "nenhum valor ímpar encontrado";
+//4. Utilizando `for` , descubra qual o menor valor contido no array e imprima-o;
+//5. Utilizando `for` , crie um array que vá de 1 até 25 e imprima o resultado;
+//6. Utilizando o array criado no exercício anterior imprima o resultado da divisão de cada um dos elementos por `2` .
