6ab27f39256edc1ae7f1c375e6fe71066c437902
Patricia Padilha <patriciapadilhabio@gmail.com>
2022-01-14 19:17:05 -0300

    exercicios 1, 2, 3 e 4 resolvidos

--- fundamentos/bloco-7-introducao-a-javascript-es6-e-testes-unitarios/dia-3-primeiros-passos-em-jest/myFizzBuzz.js
+++ fundamentos/bloco-7-introducao-a-javascript-es6-e-testes-unitarios/dia-3-primeiros-passos-em-jest/myFizzBuzz.js
@@ -1,17 +1,15 @@
 // Praticando a implementação de testes
 
 // Você vai implementar vários testes em contextos diferentes a fim de consolidar a mecânica e também a forma de pensar em testes.
-
 // Copie as funções já implementadas e desenvolva os testes. Separe as funções e os testes em arquivos diferentes para evitar qualquer tipo de problema.
+// Crie os arquivos com o respectivo nome da função.
 
-// Crie os arquivos com o respectivo nome da função. Ex: sum.js e sum.test.js
-
-function sum(a, b) {
-    if (typeof a !== 'number' || typeof b !== 'number') {
-      throw new Error('parameters must be numbers');
-    }
-  
-    return a + b;
+function myFizzBuzz(num) {
+    if (typeof num !== 'number') return false;
+    if (num % 3 === 0 && num % 5 === 0) return 'fizzbuzz';
+    if (num % 3 === 0) return 'fizz';
+    if (num % 5 === 0) return 'buzz';
+    return num;
   }
 
- module.exports = sum; 
+module.exports = myFizzBuzz;  
--- fundamentos/bloco-7-introducao-a-javascript-es6-e-testes-unitarios/dia-3-primeiros-passos-em-jest/myRemove.js
+++ fundamentos/bloco-7-introducao-a-javascript-es6-e-testes-unitarios/dia-3-primeiros-passos-em-jest/myRemove.js
@@ -1,17 +1,18 @@
 // Praticando a implementação de testes
 
 // Você vai implementar vários testes em contextos diferentes a fim de consolidar a mecânica e também a forma de pensar em testes.
-
 // Copie as funções já implementadas e desenvolva os testes. Separe as funções e os testes em arquivos diferentes para evitar qualquer tipo de problema.
+// Crie os arquivos com o respectivo nome da função.
 
-// Crie os arquivos com o respectivo nome da função. Ex: sum.js e sum.test.js
-
-function sum(a, b) {
-    if (typeof a !== 'number' || typeof b !== 'number') {
-      throw new Error('parameters must be numbers');
+// myRemove.js
+function myRemove(arr, item) {
+    let newArr = [];
+    for (let index = 0; index < arr.length; index += 1) {
+      if (item !== arr[index]) {
+        newArr.push(arr[index]);
+      }
     }
-  
-    return a + b;
+    return newArr;
   }
 
- module.exports = sum; 
+  module.exports = myRemove;
