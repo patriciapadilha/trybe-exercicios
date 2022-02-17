4bc4accc90d07f4150cd364bd8d5ea9a5cdfe0d6
Patricia Padilha <patriciapadilhabio@gmail.com>
2022-02-17 11:03:24 -0300

    atividade para fixar

---
+++ front-end/dia-1-hello-world-no-react/para-fixar/src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+import { Component } from 'react';
+
+function App() {
+  return (
+    <div>
+      <p>Nome</p>
+      <h1>Descrição</h1>
+    </div>
+  );
+}
+
+export default App;
---
+++ front-end/dia-1-hello-world-no-react/para-fixar/src/Component.js
@@ -0,0 +1,17 @@
+import { Component } from "react";
+import App from "./App";
+
+class elemento extends Component {
+    render () {
+        return (
+            <div>
+              <p>Nome</p>
+              <h1>Descrição</h1>
+            </div>
+        );
+
+    }
+}
+      
+
+export default Component;
