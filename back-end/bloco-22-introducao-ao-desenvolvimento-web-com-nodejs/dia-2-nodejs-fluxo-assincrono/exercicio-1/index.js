0e931da7d1a005395d9c3e687e7eb8b484f29cf6
Patricia Padilha <patriciapadilhabio@gmail.com>
2022-06-08 15:18:40 -0300

    exercicio 1 - promise

---
+++ back-end/bloco-22-introducao-ao-desenvolvimento-web-com-nodejs/dia-2-nodejs-fluxo-assincrono/exercicio-1/index.js
@@ -0,0 +1,34 @@
+
+// 1- Crie uma função que receba três parâmetros e retorna uma Promise.
+
+// Caso algum dos parâmetros recebidos não seja um número, rejeite a Promise com o motivo "Informe apenas números".
+
+// Caso todos os parâmetros sejam numéricos, some os dois primeiros e multiplique o resultado pelo terceiro ((a + b) * c).
+
+// Caso o resultado seja menor que 50, rejeite a Promise com o motivo "Valor muito baixo"
+
+// Caso o resultado seja maior que 50, resolva a Promise com o valor obtido.
+
+function manipulandoParametros (param1, param2, param3) {
+   const promise = new Promise((resolve, reject) => {
+       if(typeof param1 !== "number" || typeof param1 !== "number" || typeof param1 !== "number") {
+           reject(new Error("Informe apenas números"));
+       }
+
+       const resultado = ((param1 + param2) * param3);
+       
+       if (resultado < 50) {
+           reject(new Error("Valor muito baixo"));
+       }
+
+       if(resultado > 50) {
+           resolve(resultado);
+       }
+       
+   });
+   return promise;
+}
+
+manipulandoParametros(1, 1, 1)
+  .then((result) => console.log(result)) 
+  .catch(err => console.log("erro: %s", err.message))
