49239cce764d356fbdc20be3d1fab29bb02af3e0
Patricia Padilha <patriciapadilhabio@gmail.com>
2022-07-07 15:16:39 -0300

    Adiciona migration e model

---
+++ back-end/bloco-24-nodejs-orm-e-jwt/dia-1-orm-livros/models/Book.js
@@ -0,0 +1,14 @@
+const Book = (sequelize, DataTypes) => {
+    const BookTable = sequelize.define("Book", {
+      id: DataTypes.INTEGER,
+      title: DataTypes.STRING,
+      author: DataTypes.STRING,
+      pageQuantity: DataTypes.INTEGER,
+      createdAt: DataTypes.DATE,
+      updatedAt: DataTypes.DATE,
+    });
+  
+    return BookTable;
+  };
+  
+  module.exports = Book;
