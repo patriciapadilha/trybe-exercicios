92c9de9a8534eba9da5e296b4d5424eeda13a426
Patricia Padilha <patriciapadilhabio@gmail.com>
2022-07-11 12:57:47 -0300

    gitignore funcionando

--- back-end/bloco-24-nodejs-orm-e-jwt/dia-1-orm-livros/config/config.js
+++ back-end/bloco-24-nodejs-orm-e-jwt/dia-1-orm-livros/config/config.js
@@ -8,18 +8,18 @@ module.exports = {
     host: process.env.HOSTNAME,
     dialect: 'mysql',
   },
-  test: {
-    username: process.env.MYSQL_USER,
-    password: process.env.MYSQL_PASSWORD,
-    database: process.env.MYSQL_DATABASE,
-    host: process.env.HOSTNAME,
-    dialect: 'mysql',
-  },
-  production: {
-    username: process.env.MYSQL_USER,
-    password: process.env.MYSQL_PASSWORD,
-    database: process.env.MYSQL_DATABASE,
-    host: process.env.HOSTNAME,
-    dialect: 'mysql',
-  },
+  // test: {
+  //   username: process.env.MYSQL_USER,
+  //   password: process.env.MYSQL_PASSWORD,
+  //   database: process.env.MYSQL_DATABASE,
+  //   host: process.env.HOSTNAME,
+  //   dialect: 'mysql',
+  // },
+  // production: {
+  //   username: process.env.MYSQL_USER,
+  //   password: process.env.MYSQL_PASSWORD,
+  //   database: process.env.MYSQL_DATABASE,
+  //   host: process.env.HOSTNAME,
+  //   dialect: 'mysql',
+  // },
 };
